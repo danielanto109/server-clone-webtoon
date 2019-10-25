343087abed770e5d08e532abb7c5f29351019a5d
Mahesa <danielanto109@gmail.com>
2019-10-25 18:48:47 +0700

    repairing migrations

--- migrations/20191014125445-create-webtoons.js
+++ migrations/20191014125445-create-webtoons.js
@@ -32,9 +32,11 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
+        type: Sequelize.DATE
       }
     });
   },
